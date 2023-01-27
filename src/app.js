4709c497c1607b1ce8664acd8d0b6817c9afedb6
smilg-m <migluze@gmail.com>
2023-01-27 10:14:54 +0200

    added "back" functionality

--- src/app.js
+++ src/app.js
@@ -57,6 +57,7 @@ function updateCity(event) {
             "h:mm:ss"
           )}<small> ${cityTime.format("A")}</small></div>
         </div>
+        <a href="/" class="back">Back</a>
         `;
 }
 
